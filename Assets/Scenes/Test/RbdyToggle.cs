d6294d86d7b86b7472f761211cba90a787d8f10c
santiago-luhring <santiago1998@gmail.com>
2023-12-21 22:40:21 -0300

    performance fix + tree collision

--- Assets/Scenes/Test/RbdyToggle.cs
+++ Assets/Scenes/Test/RbdyToggle.cs
@@ -6,14 +6,19 @@ public class RbdyToggle : MonoBehaviour
 {
     private Rigidbody rb;
     private CollisionManager collisionManager;
-    public float delay = 0.5f;
+
+    public float stopThreshold = 0.1f; // Adjust the threshold for considering the object stopped
+    public float deactivationDuration = 3f; // Adjust the duration for considering the object stopped
+    private float timeBelowThreshold = 0f; // Timer for tracking duration below the threshold
+
+    public LayerMask groundLayer; // Specify the ground layer
 
     void Start()
     {
         // Get the Rigidbody component
         rb = GetComponent<Rigidbody>();
 
-        // Find the CollisionManager in the scene
+        // Find the CollisionManager in the scene using FindObjectOfType
         collisionManager = FindObjectOfType<CollisionManager>();
 
         // Register this object with the CollisionManager
@@ -28,18 +33,43 @@ public class RbdyToggle : MonoBehaviour
         // Check if the collision is with the specified target GameObject
         if (collision.gameObject.CompareTag("Ground"))
         {
-            // Start the DelayedDeactivation coroutine
-            StartCoroutine(DelayedDeactivation());
+            // Deactivate the Rigidbody immediately on collision
+            DeactivateRigidbody();
+        }
+
+        // Check if the collision is with the ground (based on the specified ground layer)
+        if ((groundLayer.value & 1 << collision.gameObject.layer) != 0)
+        {
+            // Deactivate the Rigidbody on collision with the ground
+            DeactivateRigidbody();
         }
     }
 
-    // Coroutine for delayed deactivation
-    IEnumerator DelayedDeactivation()
+    void Update()
     {
-        // Wait for the specified delay
-        yield return new WaitForSeconds(delay);
+        // Check if the object's velocity is below the stop threshold
+        if (rb != null && rb.velocity.magnitude < stopThreshold)
+        {
+            // Increment the timer for tracking duration below the threshold
+            timeBelowThreshold += Time.deltaTime;
 
-        // Deactivate the Rigidbody
+            // Check if the duration below the threshold exceeds the deactivation duration
+            if (timeBelowThreshold >= deactivationDuration)
+            {
+                // Deactivate the Rigidbody if the duration threshold is reached
+                DeactivateRigidbody();
+            }
+        }
+        else
+        {
+            // Reset the timer if the velocity is above the threshold
+            timeBelowThreshold = 0f;
+        }
+    }
+
+    // Method to deactivate the Rigidbody
+    public void DeactivateRigidbody()
+    {
         if (rb != null)
         {
             rb.isKinematic = true;
