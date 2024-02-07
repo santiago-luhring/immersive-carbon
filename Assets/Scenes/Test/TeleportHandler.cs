4492c0af85e0f3a69112bedef24bbdf50d0f655c
santiago-luhring <santiago1998@gmail.com>
2024-02-06 21:34:22 -0300

    teleport
    mudei um monte de coisa, n sei se algo quebrou sem querer, fingers crossed

---
+++ Assets/Scenes/Test/TeleportHandler.cs
@@ -0,0 +1,40 @@
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Teleport;
+using UnityEngine;
+
+public class TeleportHandler: MonoBehaviour, IMixedRealityTeleportHandler
+{
+    public void OnTeleportCanceled(TeleportEventData eventData)
+    {
+        Debug.Log("Teleport Cancelled");
+    }
+
+    public void OnTeleportCompleted(TeleportEventData eventData)
+    {
+        Debug.Log("Teleport Completed");
+    }
+
+    public void OnTeleportRequest(TeleportEventData eventData)
+    {
+        Debug.Log("Teleport Request");
+    }
+
+    public void OnTeleportStarted(TeleportEventData eventData)
+    {
+        Debug.Log("Teleport Started");
+    }
+
+    void OnEnable()
+    {
+        // This is the critical call that registers this class for events. Without this
+        // class's IMixedRealityTeleportHandler interface will not be called.
+        CoreServices.TeleportSystem.RegisterHandler<IMixedRealityTeleportHandler>(this);
+    }
+
+    void OnDisable()
+    {
+        // Unregistering when disabled is important, otherwise this class will continue
+        // to receive teleportation events.
+        CoreServices.TeleportSystem.UnregisterHandler<IMixedRealityTeleportHandler>(this);
+    }
+}
