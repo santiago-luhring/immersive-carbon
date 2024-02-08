a8f3a4294a73601fc504064540ffd22e249c00f2
I531069 <henrique.barboza@sap.com>
2024-02-08 00:20:42 -0300

    adding more data to the stuff

--- Assets/Scenes/Test/Country.cs
+++ Assets/Scenes/Test/Country.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class Country
 {
     public string countryName;
-    public float low;
-    public float medium;
-    public float high;
+    public float tier1;
+    public float tier2;
+    public float tier3;
+    public float tier4;
+    public float tier5;
+    public float tier6;
+    public float tier7;
+    public float tier8;
+    public float tier9;
+    public float tier10;
 
-    public Country(string aName, float aLow, float aMedium, float aHigh){
+    public Country(string aName, float aTier1, float aTier2, float aTier3, float aTier4,float aTier5, float aTier6, float aTier7, float aTier8, float aTier9, float aTier10 ){
         countryName = aName;
-        low = aLow;
-        medium = aMedium;
-        high = aHigh;
+        tier1 = aTier1;
+        tier2 = aTier2;
+        tier3 = aTier3;
+        tier4 = aTier4;
+        tier5 = aTier5;
+        tier6 = aTier6;
+        tier7 = aTier7;
+        tier8 = aTier8;
+        tier9 = aTier9;
+        tier10 = aTier10;
     }
 }
--- Assets/Scenes/Test/DataLoader.cs
+++ Assets/Scenes/Test/DataLoader.cs
@@ -30,33 +30,68 @@ public class DataLoader : MonoBehaviour
 
             string country = values[0];
             Debug.Log(values[0]);
-            float low = float.Parse(values[1]);
+            float tier1 = float.Parse(values[1]);
             Debug.Log(values[1]);
-            float medium = float.Parse(values[2]);
+            float tier2 = float.Parse(values[2]);
             Debug.Log(values[2]);
-            float high = float.Parse(values[3]);
+            float tier3 = float.Parse(values[3]);
             Debug.Log(values[3]);
+            float tier4 = float.Parse(values[4]);
+            Debug.Log(values[4]);
+            float tier5 = float.Parse(values[5]);
+            Debug.Log(values[5]);
+            float tier6 = float.Parse(values[6]);
+            Debug.Log(values[6]);
+            float tier7 = float.Parse(values[7]);
+            Debug.Log(values[7]);
+            float tier8 = float.Parse(values[8]);
+            Debug.Log(values[8]);
+            float tier9 = float.Parse(values[9]);
+            Debug.Log(values[9]);
+            float tier10 = float.Parse(values[10]);
+            Debug.Log(values[10]);
 
-            Country newCountry = new Country(country,low,medium,high);
+            Country newCountry = new Country(country,tier1,tier2,tier3,tier4,tier5,tier6,tier7,tier8,tier9,tier10);
             countryList.Add(newCountry);
-            Debug.Log(newCountry.countryName + " " + newCountry.low + " " + newCountry.medium + " " + newCountry.high );
+            Debug.Log(newCountry.countryName + " " + newCountry.tier1 + " " + newCountry.tier2 + " " + newCountry.tier3 + " " + newCountry.tier4 + " " + newCountry.tier5 + " " + newCountry.tier6 + " " + newCountry.tier7 + " " + newCountry.tier8 + " " + newCountry.tier9 + " " + newCountry.tier10 );
             
         }
         countriesData = countryList;
         return countryList;
     }
 
-    public int getRockCount(string name, int income){
+    public int getRockCount(string name, int tier){
         Country country = getCountry(name);
-        switch(income) {
+        switch(tier) {
             case 1:
-                return countRocks(country.low);
+                return countRocks(country.tier1);
                 break;
             case 2:
-                return countRocks(country.medium);
+                return countRocks(country.tier2);
                 break;
             case 3:
-                return countRocks(country.high);
+                return countRocks(country.tier3);
+                break;
+            case 4:
+                return countRocks(country.tier4);
+                break;
+            case 5:
+                return countRocks(country.tier5);
+                break;
+            case 6:
+                return countRocks(country.tier6);
+                break;
+            case 7:
+                return countRocks(country.tier7);
+                break;
+            case 8:
+                return countRocks(country.tier8);
+                break;
+            case 9:
+                return countRocks(country.tier9);
+                break;
+            case 10:
+                return countRocks(country.tier10);
                 break;
             default:
                 return 0;
